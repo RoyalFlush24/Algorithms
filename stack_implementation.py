4af6f1d553cb4ad88b89b8c2a37780faea8284b2
anumsh <anumsharma09@gmail.com>
2017-04-08 23:45:18 -0700

    fix: Fix stack implementation code

--- stack_implementation.py
+++ stack_implementation.py
@@ -1,23 +1,28 @@
 class Stack:
-  def __init__(self):
-    self.items = []
-	
-  def isEmpty(self):
-	   return self.items == []
-	
-	def push(self, item):
-    self.items.insert(0,item)
-	
-	def pop(self):
-	   return self.items.pop(0)
-	
-	def peek(self):
-	   return self.items[0]
-	
-	def size(self):
-	   return len(self.items)
-	
-	s = Stack()
-	s.push('hello')
-	s.push('true')
-	print(s.pop())
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+
+s = Stack()
+s.push('one')
+s.push('two')
+s.push('three')
+print s.size()
+print s.peek()
+print(s.pop())
+print s.peek()
+print s.isEmpty()
