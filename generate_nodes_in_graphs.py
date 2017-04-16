ef928c9d1edf395b5f105bbff46b2a51d6baa768
anumsh <anumsharma09@gmail.com>
2017-04-15 23:23:47 -0700

    feat: Genrate the list of nodes in graphs

---
+++ generate_nodes_in_graphs.py
@@ -0,0 +1,23 @@
+# how to represent graph 
+
+graph = { "a" : ["c"],
+          "b" : ["c", "e"],
+          "c" : ["a", "b", "d", "e"],
+          "d" : ["c"],
+          "e" : ["c", "b"],
+          "f" : []
+        }
+        
+#print graph
+
+# function to generate list of all edges
+
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
+
+    return edges
+
+print(generate_edges(graph))
